7791d1f4e6dd9042ce1d0836a7171c45df31b59e
BJ Clark <bjclark13@outlook.com>
2021-08-27 09:03:49 -0400

    Push inital demo

---
+++ server.js
@@ -0,0 +1,19 @@
+const express = require("express");
+const app = express();
+const port = 3000;
+
+let counter = 0;
+
+app.get("/", (req, res) => {
+  counter++;
+  if (counter > 10) {
+    server.close();
+    res.send(`dead.`);
+  }
+
+  res.send(`Hello World! Count ${counter}.`);
+});
+
+var server = app.listen(port, () => {
+  console.log(`Example app listening at http://localhost:${port}`);
+});
